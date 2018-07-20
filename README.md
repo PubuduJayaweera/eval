1e993c850b4089ab92989acf179b11003c7a27e6
Avon <Avon>
2018-07-20 20:45:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# EvalBot
+* Clone repo to anywhere and rename settings.js_example to settings.js
+* configure settings.js as explained in the file
+* Make sure you have nodejs 6.6.0 installed
+* cd to the bots directory  in your command line
+* run `npm install`
+* run `npm install forever -g`
+* run `forever bot.js`
+* ???
+* profit
---
+++ bot.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const auth = require('./settings.js');
+
+bot.on('message', (message) => {
+//    if (message.author.id !== bot.user.id) return;
+    if (!message.content.startsWith(auth.prefix)) return;
+    let command = message.content.slice(auth.prefix.length);
+    let split = command.split(" ");
+    command = split[0];
+    split.shift();
+    let code = split.join(" ");
+//    if (command === "eval") {
+        try {
+            console.log('got it');
+        let ev = require('util').inspect(eval(code));
+        if (ev.length > 1950) {
+            ev = ev.substr(0, 1950);
+        }
+        let token = auth.token.replace(/\./g, "\.")
+        let re = new RegExp(token, 'g') 
+        ev = ev.replace(re, "*R-eD-Ac-Te-D-*");
+        message.channel.send("**Input:**```js\n"+code+"```**Eval:**```js\n"+ev+"```")
+        } catch(err) {
+            message.channel.send('```js\n'+err+"```")
+        }
+//    }
+})
+
+bot.on('ready', () => {
+    console.log("Ready To Eval")
+})
+
+bot.login(auth.token)
