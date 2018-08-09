6b64f08648868c3434aff4481190f7cca86cb964
PubuduJayaweera <40852358+PubuduJayaweera@users.noreply.github.com>
2018-08-10 03:20:51 +0530

    Delete settings.js

--- settings.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  token:"", //Your user token which you can get from running `localStorage.token` in the devloper console in discord.
-  prefix:"ev" //The prefix for the bot.
-  };
